43dae73aa29f271620f7f3bc5e2688b0ae47b972
Debarjun <debarjunthakur020@gmail.com>
2024-04-20 00:41:02 +0530

    UI changes

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ##TODO
 
-- [] Make it Deploy
-- [] Scaffold Basic UI with data
+- [X] Make it Deploy
+- [X] Scaffold Basic UI with data
 - [] Actually set up a database
 - [] Attach database to UI
 - [] Add authentication
 - [] Add image upload
-- [] Error Management
+- [] Error Management 
 - [] Routing/ image page
 - [] Delete button
 - [] Analytics
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/a16af2c8-8266-4faa-b770-b4c52f61d4cf-2hu7kj.jpg",
+  "https://utfs.io/f/3e883d6d-aca1-434e-a6c0-d11419a65091-wwgqun.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))
+      }</div>
     </main>
   );
 }
