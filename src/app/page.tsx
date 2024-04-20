48d78324e5b5c9de9afc71a96030e9389c431ba9
Debarjun <debarjunthakur020@gmail.com>
2024-04-20 20:20:35 +0530

    authorization in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,33 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images () {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">{
+      [...images, ...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + '-'+ index} className="w-48 flex flex-col">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))
+    }</div>
+  );
+}
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">{
-        [...images, ...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + '-'+ index} className="w-48 flex flex-col">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))
-      }</div>
+      <SignedOut>
+        <div className="w-full h-full flex flex-col items-center justify-center">Please sign in to view images</div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
